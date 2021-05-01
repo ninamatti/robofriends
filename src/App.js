fe0280d433e5aa835775600f6e6f7daad5c93825
Nina Matti <ninadesireematti@gmail.com>
2021-05-01 06:48:17 +0200

    Added constructor, app state and first event function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import Searchbox from './Searchbox';
+import { robots } from './robots';
 
 
-const App = ({ robots }) => {
-    return (
-        <div className='tc'>
-            <h1>Robofriends</h1>
-            <Searchbox />
-            <CardList robots={robots}/>
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        };
+    }
+
+    onSearchChange(event) {
+        console.log(event);
+    }
+
+    render() {
+        return (
+            <div className='tc'>
+                <h1>Robofriends</h1>
+                <Searchbox />
+                <CardList robots={this.state.robots}/>
+            </div>
+        )
+    }
 }
 
 export default App;
