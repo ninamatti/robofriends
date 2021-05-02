fb1ad738b24c3fc6fed4fe4e9e347a6b13fe2ac6
Nina Matti <ninadesireematti@gmail.com>
2021-05-02 07:03:43 +0200

    Created folder structure, moved files accordingly and adjusted imports

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import Searchbox from '../components/Searchbox';
+import './App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => res.json())
+            .then((users) => this.setState({ robots: users })
+            );
+        
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value})
+    };
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        });
+        if(!robots.length) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className="f1">Robofriends</h1>
+                    <Searchbox searchChange={this.onSearchChange}/>
+                    <Scroll >
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundry>
+                    </Scroll>
+                </div>
+            ) 
+        }
+        
+    }
+}
+
+export default App;
